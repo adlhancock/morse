5b33d568a9826dd7ee78988e64e82e7ff1daf7ee
David Hancock <david@adlhancock.net>
2018-07-20 10:52:13 +0100

    better options and output

--- tests/newsticker_test.py
+++ tests/newsticker_test.py
@@ -13,24 +13,33 @@ from morse.newsticker import get_news
 from morse.morse import translate
 from morse.morse import morsetowav
 
-import winsound
+#import winsound
 
 #%%
+print("getting news...")
 news = get_news()
+print("...got news!")
 
 plain_titles = news["titles"]
+
+print("translating titles to morse")
 morse_titles = [translate(x) for x in plain_titles]
+
+print("retranslating morse titles to plaintext")
 retranslated_titles = [translate(x,"morse") for x in morse_titles]
 
-print("MORSE HEADLINES\n"+"*"*50)
-[print(translate(x)+"\n"+"="*50+"\n") for x in morse_titles]
-
-print("RETRANSLATION CHECK:\n"+"*"*50)
-[print(x.upper()+"\n"+y+"\n") for x,y in zip(plain_titles,retranslated_titles)]
-
-#%%
-
-morsetowav(morse_titles[-1],"news.wav",verbose=True)
+response = input("print morse headlines [Y/n] ")
+if response in ["Y",""]:
+    print("MORSE HEADLINES\n"+"*"*50)
+    [print(translate(x)+"\n"+"="*50+"\n") for x in morse_titles]
 
 
+response = input("print retranslation check [Y/n] ")
+if response in ["Y",""]:
+    print("RETRANSLATION CHECK:\n"+"*"*50)
+    [print(x.upper()+"\n"+y+"\n") for x,y in zip(plain_titles,retranslated_titles)]
+
+response = input("record wav file [Y/n] ")
+if response in ["Y",""]:
+    morsetowav(morse_titles[-1],"news.wav",verbose=True)
 
